a71cfab1473913e40c992b8bcf9f291a3a62eea0
Lenia03 <s244927@student.dtu.dk>
2026-09-11 23:02:59 +0200

    Add conveyor belt weight check functionality

--- src/main.py
+++ src/main.py
@@ -1,1 +1,7 @@
-print("hello!")
+motors = int(input())
+weight=int(input())
+weight_per_motor=weight/motors
+if weight_per_motor<=12:
+    print("Yes! The conveyor belt can carry the packages.")
+else:
+    print("No! The conveyor belt cannot carry the packages.")   
